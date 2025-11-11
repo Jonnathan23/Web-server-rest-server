a2d6ad64833befbaf84f07f87e13b74e0aa97d10
Jonnathan23 <jsaquicela26@gmail.com>
2025-11-10 23:25:22 -0500

    implementacion de reenderizado el contenido de nuetra carpeta public y redireccion de la url

---
+++ src/app/presentation/server.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import path from 'path';
+
+export class Server {
+    private app = express();
+
+    async start() {
+
+        //* Middlewares
+
+        //* Public Folder
+        this.app.use(express.static('public'));
+
+        this.app.get(/.*/, (req, res) => {
+            const indexPath = path.join(__dirname + '../../../../public/index.html');
+            res.sendFile(indexPath);
+            return;
+        })
+
+
+        this.app.listen(3000, () => {
+            console.log(`Server running on port ${3000}`)
+        })
+    }
+}
